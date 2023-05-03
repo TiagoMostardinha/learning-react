edc133b5007014ecbd9aa0121bdc8c88c1772782
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-03 12:08:29 +0100

    5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
+import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = "http://www.google.com"
+
 
   return (
     <div className="App">
+      <NavBar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked {likes} times.</p>
-
-        <p>{10}</p>
-        <p>{"hello world"}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Website</a>
+        <Home />
       </div>
     </div>
   );
