67c5cae53e044dccd084bf328e84cdb334669bea
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-03 14:43:00 +0100

    7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Button was clicked!');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
+    const handleClickAgainAgain = (name,e) => {
+        console.log('hello ' + name,e);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => { handleClickAgain('mario') }}>Click me again</button>
+            <button onClick={(e) => { handleClickAgainAgain('yoshi',e) }}>Click me again again</button>
         </div>
     );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -4,13 +4,7 @@ const NavBar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-
-                {/* Inline styling css */}
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>New Blog</a>
+                <a href="/create">New Blog</a>
             </div>
         </nav>
     );
