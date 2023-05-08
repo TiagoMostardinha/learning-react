572e63af3efde42ed34292670b75bed352898cc5
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-08 12:37:35 +0100

    16

--- src/Home.js
+++ src/Home.js
@@ -30,3 +30,10 @@ const Home = () => {
 }
 
 export default Home;
+
+/* Endpoints
+/blogs          GET    Fetch all blogs
+/blogs/{id}     GET    Fetch a single blog
+/blogs          POST   Add a new blog
+/blogs/{id}     DELETE Delete a blog
+*/
