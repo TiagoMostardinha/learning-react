adca13ed8fc0194f42a008313f0b5a17c5cbd806
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-08 16:41:57 +0100

    28

--- src/Create.js
+++ src/Create.js
@@ -2,13 +2,20 @@ import { useState } from "react";
 
 const Create = () => {
     const [title, setTitle] = useState('');
-    const [body,setBody] = useState('')
-    const [author,setAuthor] = useState('mario')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('mario')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog)
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input
                     type="text"
@@ -27,15 +34,12 @@ const Create = () => {
                 <label>Blog author:</label>
                 <select
                     value={author}
-                    onChange={(e)=>setAuthor(e.target.value)}
+                    onChange={(e) => setAuthor(e.target.value)}
                 >
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
             </form>
         </div>
     );
